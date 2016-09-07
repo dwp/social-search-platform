c761b2e4078724fadadc9756ab0fb01f722933f3
Jonty Bale <jonty.bale@dwp.gsi.gov.uk>
2016-09-07 14:33:00 +0100

    Improving the install script and setting up basic env dist file.

--- install.sh
+++ install.sh
@@ -7,22 +7,36 @@ function checkoutLatestTag {
     git checkout ${TAG} --quiet
 }
 
+# formatting
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 # get our initial data
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SLACK_APP_DIR="${SCRIPTDIR}/slack-api-export"
-SOCIAL_SEARCH_DIR="${SCRIPTDIR}/social-search"
+SLACK_APP="slack-api-export"
+SLACK_APP_DIR="${SCRIPTDIR}/${SLACK_APP}"
+SOCIAL_SEARCH="social-search"
+SOCIAL_SEARCH_DIR="${SCRIPTDIR}/${SOCIAL_SEARCH}"
 
 # install social search
+printf "Validating ${SOCIAL_SEARCH} install.\n"
 if [ ! -d "${SOCIAL_SEARCH_DIR}" ]; then
-    git clone git@gitlab.itsshared.net:Innovation/social-search.git ${SOCIAL_SEARCH_DIR}
+    git clone git@gitlab.itsshared.net:Innovation/${SOCIAL_SEARCH}.git ${SOCIAL_SEARCH_DIR}
 fi
 checkoutLatestTag ${SOCIAL_SEARCH_DIR}
 
 # install slack-api-export
+printf "Validating ${SLACK_APP} install.\n"
 if [ ! -d "${SLACK_APP_DIR}" ]; then
-    git clone git@gitlab.itsshared.net:Innovation/slack-api-export.git ${SLACK_APP_DIR}
+    git clone git@gitlab.itsshared.net:Innovation/${SLACK_APP}.git ${SLACK_APP_DIR}
 fi
 checkoutLatestTag ${SLACK_APP_DIR}
+
+# check .env is setup
+if [ ! -f '${SCRIPTDIR}/.env' ]; then
+    printf "${RED}Error:${NC} docker-compose .env file is not found, please complete project setup as per readme.md before running this file.\n"
+    exit
+fi
 
 # run docker-compose for our own build
 cd ${SCRIPTDIR}
